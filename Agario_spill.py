4f7608b7b90ffc4902ca4d96e26e25f94a3bafd4
jensemannDJ <jensbl@viken.no>
2023-11-23 13:15:54 +0100

    23 nomember

--- Agario_spill.py
+++ Agario_spill.py
@@ -1,18 +1,38 @@
-import pygame
+import pygame,random
 
 # klasser
 class Matbit:
     def __init__(self):
-        self.bilde = pygame.image.load("").convert_alpha()
-        self.ramme = self.bilde.get.rect()
-        self.ramme.centerx = 100
-        self.ramme.centery = 200 
+        self.bilde = pygame.image.load("bilder/beer.png").convert_alpha()
+        self.ramme = self.bilde.get_rect()
+        self.ramme.centerx = random.randint(0,BREDDE)
+        self.ramme.centery = random.randint(0,HOYDE)
 
     def tegn(self,vindu):
         vindu.blit(self.bilde,self.ramme)
 
-#1oppsett
+class Celle:
+    def __init__(self,navn:str,radius:int,bildesti:str):
+        self.navn = navn 
+        self.radius = radius
+        self.bilde = pygame.image.load(bildesti)
+        self.ramme = self.bilde.get_rect()
 
+    def beveg(self):
+        pass
+
+    def spis(self):
+        pass
+
+    def tegn(self,vindu):
+        vindu.blit(self.bilde,self.ramme)
+        
+
+    
+
+
+
+#1.oppsett
 pygame.init()
 BREDDE=600
 HOYDE=600
@@ -20,7 +40,10 @@ FPS=60
 vindu= pygame.display.set_mode((BREDDE, HOYDE)) 
 klokke = pygame.time.Clock()
 
+Stag = Celle("Stag",50,"bilder/stag.jpeg")
+Jets = Celle("Jets",25,"bilder/jets.jpeg")  
 matbit1=Matbit()
+matbit1 = Matbit()
 matbit2=Matbit()
 
 while True:
@@ -32,7 +55,9 @@ while True:
 
     # 3. oppdatere spill
     # 4. tegn
-    vindu 
+    vindu.fill("white")
+    Stag.tegn(vindu)
+    Jets.tegn(vindu)
     matbit1.tegn(vindu)
     matbit2.tegn(vindu)
 
